87b0c8abca4c5724ed3b6a78078661a0ec96bc4d
Tanmayee P <may010602@gmail.com>
2022-03-09 10:05:41 +0530

    Added all current work.

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import GoogleLogin from 'react-google-login';
 import { useNavigate } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc';
-import back from '../assets/logi';
+import back from '../assets/login.jpg';
 const login = () => {
   return <div className='flex justify-start items-center flex-col h-screen'>
-    <div className='relative w-full h-full'>
-      <image src={back} className= 'w-full h-full object-cover'/>
-      hello
+    <div className='relative w-full h-full' style={{backgroundImage : "..assets/login.png"}}>
+      Last done till 41:00 (https://youtu.be/1RHDhtbqo94)
     </div>
   </div>;
 };
